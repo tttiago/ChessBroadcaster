bddb942ffe75cafa90a04618b8479942c91dcddb
tiago <tiagosf99@gmail.com>
2023-04-26 20:21:13 +0100

    Copied calibration functions from Duarte. Still not working.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 from collections import deque
 
 import cv2
+import imutils
 import numpy as np
 
 from board_basics import BoardBasics
@@ -93,6 +94,7 @@ pts1 = np.float32(
         list(corners[8][8]),
     ]
 )
+print(pts1)
 
 
 def waitUntilMotionCompletes():
@@ -100,6 +102,7 @@ def waitUntilMotionCompletes():
     counter = 0
     while counter < COUNTER_MAX_VALUE:
         frame = video_capture_thread.get_frame()
+        # frame = corcamera(frame)
         frame = perspective_transform(frame, pts1)
         fgmask = motion_fgbg.apply(frame)
         ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
@@ -115,6 +118,7 @@ def stabilize_background_subtractors():
     counter = 0
     while counter < COUNTER_MAX_VALUE:
         frame = video_capture_thread.get_frame()
+        # frame = corcamera(frame)
         frame = perspective_transform(frame, pts1)
         move_fgbg.apply(frame)
         fgmask = motion_fgbg.apply(frame, learningRate=0.1)
@@ -130,6 +134,7 @@ def stabilize_background_subtractors():
     counter = 0
     while counter < COUNTER_MAX_VALUE:
         frame = video_capture_thread.get_frame()
+        # frame = corcamera(frame)
         frame = perspective_transform(frame, pts1)
         fgmask = move_fgbg.apply(frame, learningRate=0.1)
         ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
@@ -144,6 +149,118 @@ def stabilize_background_subtractors():
     return frame
 
 
+################### NEW FUNCTIONS FROM DUARTE ###########################
+
+
+def corcamera(img):
+    mtx = np.matrix(
+        [[786.098795, 0.0, 704.94428], [0.0, 1145.3459778, 415.1591336], [0.0, 0.0, 1.0]]
+    )
+    dist = np.matrix([-0.4021267, 0.1650988, -0.00039044, -0.0003982488, -0.0086298372])
+    h, w = img.shape[:2]
+
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y : y + h, x : x + w]
+    img = img[y : y + h, x : x + w]
+
+    return dst
+
+
+def getboardloc_complete(mask, prevxsquare):
+    xyratio = 1.115
+    if prevxsquare == 0:
+        # xsquare = 21
+        xsquare = 30
+        minxsquare = 30
+        maxxsquare = 60
+        # minxsquare = 21
+        # maxxsquare = 46
+        # minangle = -40
+        # maxangle = 40
+        minangle = -20
+        maxangle = 20
+
+        div = 2
+    else:
+        minxsquare = prevxsquare - 4
+        maxxsquare = prevxsquare + 4
+        xsquare = minxsquare
+        minangle = -20
+        maxangle = 20
+        div = 1
+    ysquare = xsquare * xyratio
+    xx = xsquare * 8
+    yy = ysquare * 8
+    template = gen_mask(xsquare, ysquare, 0)
+    method = cv2.TM_CCOEFF_NORMED
+    rotated = imutils.rotate_bound(mask, -20)
+    res = cv2.matchTemplate(template, rotated, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    maxmax = max_val
+    top_left = max_loc
+    anglemax = -20
+    xsquaremax = xsquare
+    for xsquare in range(minxsquare, maxxsquare, 1):
+        ysquare = xsquare * xyratio
+        xx = xsquare * 8
+        yy = ysquare * 8
+        template = gen_mask(xsquare, ysquare, 0)
+
+        for angle in range(minangle, maxangle, 1):
+            angle = angle / div
+            rotated = imutils.rotate_bound(mask, angle)
+            res = cv2.matchTemplate(template, rotated, method)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            if max_val > maxmax:
+                maxmax = max_val
+                # print(maxmax)
+                anglemax = angle
+                top_left = max_loc
+                xsquaremax = xsquare
+
+    return anglemax, top_left, xsquaremax, maxmax
+
+
+def getboardloc_normal(mask, anglemax, xsquare):
+    method = cv2.TM_CCOEFF_NORMED
+    rotated = imutils.rotate_bound(mask, anglemax)
+    xyratio = 1.115
+    ysquare = xsquare * xyratio
+    xx = xsquare * 8
+    yy = ysquare * 8
+    template = gen_mask(xsquare, ysquare, 0)
+    res = cv2.matchTemplate(template, rotated, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    top_left = max_loc
+
+    return top_left, max_val
+
+
+def gen_mask(sizex, sizey, topleftcolour):
+    # Use a breakpoint in the code line below to debug your script.
+    width = round(sizex * 8)
+    height = round(sizey * 8)
+    mask = np.zeros((height, width), np.uint8)
+    # print(mask)
+    for n in range(8):
+        for m in range(8):
+            mask[
+                round(n * sizey) : round((n + 1) * sizey),
+                round(m * sizex) : round((m + 1) * sizex),
+            ] = (topleftcolour != ((n + m) % 2)) * 255
+    return mask
+
+
+######################## DUARTE VAR INIT ###############################
+
+xsquaremax = 0
+trigger = True
+
+
 #############################  Main code ################################
 
 previous_frame = stabilize_background_subtractors()
@@ -155,11 +272,68 @@ previous_frame_queue.append(previous_frame)
 while not broadcast.board.is_game_over():
     sys.stdout.flush()
     frame = video_capture_thread.get_frame()
+    # cv2.imshow("frame", frame)
+    # cv2.waitKey(0)
+
+    # frame = corcamera(frame)
+
+    # cv2.imshow("corrected_frame", frame)
+    # cv2.waitKey(0)
+    imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(imgHSV, (7, 7), 0)
+    mask = cv2.adaptiveThreshold(
+        blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 2
+    )
+    # cv2.imshow("mask", mask)
+    # cv2.waitKey(0)
+    method = cv2.TM_CCOEFF_NORMED
+
+    if trigger:
+        anglemax, top_left, xsquaremax, max_val_hist = getboardloc_complete(mask, xsquaremax)
+        print("Finish getboardloccomplete")
+        xsquare = xsquaremax
+        ysquare = 1.115 * xsquare
+        xx = xsquare * 8
+        yy = ysquare * 8
+        pts1 = np.float32(
+            [
+                list(top_left),
+                list((top_left[0], top_left[1] + yy)),
+                list((top_left[0] + xx, top_left[1])),
+                list((top_left[0] + xx, top_left[1] + yy)),
+            ]
+        )
+        trigger = False
+    else:
+        trigger = False
+        # top_left, max_val = getboardloc_normal(mask, anglemax, xsquaremax)
+        # pts1 = np.float32(
+        #     [
+        #         list(top_left),
+        #         list((top_left[0], top_left[1] + yy)),
+        #         list((top_left[0] + xx, top_left[1])),
+        #         list((top_left[0] + xx, top_left[1] + yy)),
+        #     ]
+        # )
+        # max_val_hist = max_val_hist * 0.95 + max_val * 0.05
+        # # print(max_val_hist)
+        # if max_val_hist < 0.5 or max_val < 0.25:
+        #     trigger = True
+
+    frame = imutils.rotate_bound(frame, anglemax)
     frame = perspective_transform(frame, pts1)
+    cv2.imshow("frame_depois", frame)
+    cv2.waitKey(1)
+
     fgmask = motion_fgbg.apply(frame)
+    cv2.imshow("fgmask_antes", fgmask)
+    cv2.waitKey(1)
+
     ret, fgmask = cv2.threshold(fgmask, 250, 255, cv2.THRESH_BINARY)
     kernel = np.ones((11, 11), np.uint8)
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    cv2.imshow("fgmask", fgmask)
+    cv2.waitKey(1)
     mean = fgmask.mean()
     if mean > MOTION_START_THRESHOLD:
         # cv2.imwrite("motion.jpg", fgmask)
